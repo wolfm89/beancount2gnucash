ecd314200fb528548c0535c86511b57bf4e66243
lebenlechzer <lebenlechzer@gmail.com>
2018-12-16 22:25:41 +0100

    Write accounts file

--- beancount2gnucash.py
+++ beancount2gnucash.py
@@ -3,14 +3,48 @@
 
 import argparse
 import os.path
-from beancount import loader
+import csv
+from collections import defaultdict
+from difflib import get_close_matches
+from beancount import loader, core
 
 LEDGER_FILENAME = "ledger_filename"
+GNUCASH_ACC_TYPES = {"BANK", "CASH", "ASSET",
+                     "CREDIT", "LIABILITY", "STOCK", "MUTUAL",
+                     "INCOME", "EXPENSE", "EQUITY",
+                     "RECEIVABLE", "PAYABLE", "TRADING"}
+ACC_HEADERS = {"type": "type", "full_name": "full_name", "name": "name", "code": "code",
+               "description": "description", "color": "color", "notes": "notes",
+               "commoditym": "commoditym", "commodityn": "commodityn", "hidden": "hidden",
+               "tax": "tax", "place_holder": "place_holder"}
+
 
 def main(ledger_filename):
+    head, tail = os.path.split(ledger_filename)
     entries, errors, options = loader.load_file(ledger_filename)
-    print(entries)
-    pass
+    # for entry in entries:
+    #     print(type(entry))
+    export_accounts([entry for entry in entries if isinstance(
+        entry, core.data.Open)], head, os.path.splitext(tail)[0])
+
+
+def export_accounts(accounts, directory, basename):
+    rows = []
+    for account in accounts:
+        # print(account)
+        row = defaultdict(lambda: "")
+        row[ACC_HEADERS["full_name"]] = account.account
+        row[ACC_HEADERS["name"]] = account.account.split(":")[-1]
+        row[ACC_HEADERS["type"]] = get_close_matches(
+            account.account.split(":")[0].upper(), GNUCASH_ACC_TYPES, n=1)[0]
+        row[ACC_HEADERS["commodityn"]] = account.currencies[0]
+        rows.append(row)
+    with open(basename + '_accounts.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(
+            csvfile, ACC_HEADERS.values(), quoting=csv.QUOTE_ALL)
+        for row in rows:
+            writer.writerow(row)
+
 
 def parse_args():
     def filename(x):
@@ -22,6 +56,7 @@ def parse_args():
     parser.add_argument(LEDGER_FILENAME, type=filename,
                         help="filename of beancount ledger file")
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = parse_args()
